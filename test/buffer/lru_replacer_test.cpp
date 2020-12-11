98dbc68c98abe42da347fdedb44bc0820eff0c58
awfeequdng <291458254@qq.com>
2020-12-11 19:22:40 +0800

    fix LRU Testing : lru_replacer_test.cpp(#96)

--- test/buffer/lru_replacer_test.cpp
+++ test/buffer/lru_replacer_test.cpp
@@ -35,28 +35,28 @@ TEST(LRUReplacerTest, DISABLED_SampleTest) {
   // Scenario: get three victims from the lru.
   int value;
   lru_replacer.Victim(&value);
-  EXPECT_EQ(1, value);
-  lru_replacer.Victim(&value);
   EXPECT_EQ(2, value);
   lru_replacer.Victim(&value);
   EXPECT_EQ(3, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(4, value);
 
   // Scenario: pin elements in the replacer.
-  // Note that 3 has already been victimized, so pinning 3 should have no effect.
-  lru_replacer.Pin(3);
+  // Note that 4 has already been victimized, so pinning 4 should have no effect.
   lru_replacer.Pin(4);
+  lru_replacer.Pin(5);
   EXPECT_EQ(2, lru_replacer.Size());
 
-  // Scenario: unpin 4. We expect that the reference bit of 4 will be set to 1.
-  lru_replacer.Unpin(4);
+  // Scenario: unpin 5. We expect that the reference bit of 5 will be set to 1.
+  lru_replacer.Unpin(5);
 
   // Scenario: continue looking for victims. We expect these victims.
   lru_replacer.Victim(&value);
-  EXPECT_EQ(5, value);
-  lru_replacer.Victim(&value);
   EXPECT_EQ(6, value);
   lru_replacer.Victim(&value);
-  EXPECT_EQ(4, value);
+  EXPECT_EQ(1, value);
+  lru_replacer.Victim(&value);
+  EXPECT_EQ(5, value);
 }
 
 }  // namespace bustub
